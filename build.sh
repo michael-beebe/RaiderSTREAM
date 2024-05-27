ee027fd7db75c10bc805cf5651d289047e78925b
michael-beebe <michaelbeebe@protonmail.com>
2024-05-27 08:40:32 -0700

    updated build.sh

--- build.sh
+++ build.sh
@@ -12,25 +12,36 @@ cd build
 # --- Configure the project with CMake
 #-----------------------------------------
 # --- OpenMP ---
-# cmake \
-#   -DENABLE_OMP=ON \
-#   ../
+cmake \
+  -DENABLE_OMP=ON \
+  -DCMAKE_C_FLAGS="-fopenmp" \
+  -DCMAKE_CXX_FLAGS="-fopenmp" \
+  ../
 
 # --- MPI ---
 # cmake \
 #   -DENABLE_MPI_OMP=ON \
 #   -DCMAKE_C_COMPILER=`which mpicc` \
 #   -DCMAKE_CXX_COMPILER=`which mpic++` \
+#   -DCMAKE_C_FLAGS="-fopenmp" \
+#   -DCMAKE_CXX_FLAGS="-fopenmp" \
 #   ../
 
 # --- OpenSHMEM ---
-cmake                                   \
-  -DENABLE_SHMEM_OMP=ON                 \
-  -DCMAKE_C_COMPILER=`which oshcc`      \
-  -DCMAKE_CXX_COMPILER=`which oshc++`   \
-  -DCMAKE_C_FLAGS="-fopenmp"            \
-  -DCMAKE_CXX_FLAGS="-fopenmp"          \
-  ../
+# cmake                                   \
+#   -DENABLE_SHMEM_OMP=ON                 \
+#   -DCMAKE_C_COMPILER=`which oshcc`      \
+#   -DCMAKE_CXX_COMPILER=`which oshc++`   \
+#   -DCMAKE_C_FLAGS="-fopenmp"            \
+#   -DCMAKE_CXX_FLAGS="-fopenmp"          \
+#   ../
+
+# --- CUDA ---
+# cmake \
+#   -DENABLE_CUDA=ON \
+#   -DCMAKE_CXX_COMPILER=`which nvcc` \
+#   -DCMAKE_EXE_LINKER_FLAGS="-lcudart -lcudadevrt" \
+#   ../
 
 # --- Build the project
 make
--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@
 cd ./build/bin
 
 # --- OpenMP Test
-# export OMP_NUM_THREADS=10
-# ./raiderstream -k all -s 100000 -np 1
+export OMP_NUM_THREADS=30
+./raiderstream -k all -s 100000 -np 1
 
 # --- MPI/OpenMP Test
 # mpirun -np 4 --mca btl ^openib raiderstream -k all -s 10000000 -np 4
 
 # --- OpenSHMEM/OpenMP Test
-export UCX_LOG_LEVEL=debug
-ulimit -a
-ulimit -m unlimited
-ulimit -v unlimited
-ulimit -n 4096
-export SHMEM_SYMMETRIC_HEAP_SIZE=10G
-export SHMEM_MAX_SEGMENTS=128
-oshrun --mca btl ^openib --mca opal_common_ucx_opal_mem_hooks 1 -np 1 ./raiderstream -k seq_copy -s 100000 -np 1
+# export UCX_LOG_LEVEL=debug
+# ulimit -a
+# ulimit -m unlimited
+# ulimit -v unlimited
+# ulimit -n 4096
+# export SHMEM_SYMMETRIC_HEAP_SIZE=10G
+# export SHMEM_MAX_SEGMENTS=128
+# oshrun --mca btl ^openib --mca opal_common_ucx_opal_mem_hooks 1 -np 1 ./raiderstream -k seq_copy -s 100000 -np 1
