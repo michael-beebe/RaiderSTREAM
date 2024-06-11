fdf57f252cf5365cf27094f2874bb38adc2c296f
Nicholas Anderson <and21829@gpu-20-10.localdomain>
2024-06-11 13:14:08 -0500

    Added RS_OACC implementation folder, and updated build, CMakeLists.txt,  and run scripts as well as RS_Main.cpp to be compatible with the RS_OACC folder

--- build.sh
+++ build.sh
@@ -18,6 +18,15 @@ cd build
 #   -DCMAKE_CXX_FLAGS="-fopenmp" \
 #   ../
 
+# --- OpenACC ---
+ cmake \
+   -DENABLE_OACC=ON \
+   -DCMAKE_C_COMPILER=`which nvc` \
+   -DCMAKE_CXX_COMPILER=`which nvc++` \
+   -DCMAKE_C_FLAGS="-acc -ta=tesla:cc70 -Minfo=accel" \
+   -DCMAKE_CXX_FLAGS="-acc -ta=tesla:cc70 -Minfo=accel" \
+   ../
+
 # --- MPI ---
 # cmake \
 #   -DENABLE_MPI_OMP=ON \
@@ -37,12 +46,12 @@ cd build
 #   ../
 
 # --- CUDA ---
-cmake \
-  -DENABLE_CUDA=ON \
-  -DCMAKE_C_COMPILER=`which nvcc` \
-  -DCMAKE_CXX_COMPILER=`which nvcc` \
-  -DCMAKE_EXE_LINKER_FLAGS="-lcudart -lcudadevrt" \
-  ../
+#cmake \
+#  -DENABLE_CUDA=ON \
+#  -DCMAKE_C_COMPILER=`which nvcc` \
+#  -DCMAKE_CXX_COMPILER=`which nvcc` \
+#  -DCMAKE_EXE_LINKER_FLAGS="-lcudart -lcudadevrt" \
+#  ../
 
 # --- Build the project
 make
